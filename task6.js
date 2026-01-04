4e5cba924553b83aa69a683531e92e84e976f58c
Narasgonda Patil <narasgondapatil0345@gmail.com>
2026-01-04 19:49:31 +0530

    Added task6 to javascript practice

---
+++ task6.js
@@ -0,0 +1,4 @@
+var a = 30
+console.log(a>15 && a<40)//true because both conditions are true
+console.log(a<0 || a>29)//true because one condition is true
+console.log(!(a<20))//true because condition is false and ! operator negates it to true
