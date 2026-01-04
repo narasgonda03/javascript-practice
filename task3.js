c3984927e97f6d50242cc39e140e7cc6fdbce4a0
Narasgonda Patil <narasgondapatil0345@gmail.com>
2026-01-04 19:41:19 +0530

    Added task3 for javascript practice

---
+++ task3.js
@@ -0,0 +1,15 @@
+let num = 10;
+let name = "JavaScript";
+let isTrue = true;
+let x;
+let y = null;
+let arr = [1, 2, 3];
+let obj = { city: "Kolhapur" };
+
+console.log(typeof num);// number
+console.log(typeof name);//string
+console.log(typeof isTrue);//boolean value
+console.log(typeof x);//undefined
+console.log(typeof y);      // object
+console.log(typeof arr);    // object
+console.log(typeof obj);// object
