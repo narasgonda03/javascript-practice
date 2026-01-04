b140676786ea613d9a84fac2ee5697c8f9db10c4
Narasgonda Patil <narasgondapatil0345@gmail.com>
2026-01-04 19:54:23 +0530

    Added task8 to javascript practice

---
+++ task8.js
@@ -0,0 +1,6 @@
+var a = 28
+if(a%2==0){//checking whether a is even
+    console.log("The number is even")
+}else{//if it not even then this block will execute
+    console.log("The number is odd")
+}
