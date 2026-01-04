54e4f73877bc6df88c8d4fe1bf26877cc4ea0c14
Narasgonda Patil <narasgondapatil0345@gmail.com>
2026-01-04 19:29:00 +0530

    Updated code of task1 javascript file

--- task1.js
+++ task1.js
@@ -1,4 +1,13 @@
 var a = 10//declarating and intializing variable a
 console.log(a)
-a = 20//reassigning variable a
+var a = 20//redeclarating and reassigning variable a in var
 console.log(a)
+
+let b = 30//declarating and intializing variable b
+console.log(b)
+b = 40//don't redeclarate variable b but reassigning variable b in let
+console.log(b)
+
+const c = 50
+c=60//don't redeclarate variable c and don't reassigning variable c in const
+console.log(c)
