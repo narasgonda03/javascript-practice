398cb3ab9c208b5c5de7a1fd54a75e1fc3d52739
Narasgonda Patil <narasgondapatil0345@gmail.com>
2026-01-04 19:43:41 +0530

    Added task4 for javascript practice

---
+++ task4.js
@@ -0,0 +1,7 @@
+var a = 30
+var b = 10
+console.log(a+b)//addition of a and b
+console.log(a-b)//subtraction of a and b
+console.log(a*b)//multiplication of a and b
+console.log(a/b)//division of a and b
+console.log(a%b)//modulus of a and b
