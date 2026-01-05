ec1c5cefe4ec9795b265d50f807ad3b18bdb9e34
Narasgonda Patil <narasgondapatil0345@gmail.com>
2026-01-05 18:27:26 +0530

    Nested-If program is added

---
+++ nestedIf.js
@@ -0,0 +1,8 @@
+let num = 12
+if(num>0){//check the given number is greater than zero.
+    if(num%2==0){//in this nested if it checks the given number exactly divisible by 2.
+        console.log("Number is Positive Even Number.")
+    }
+}else{//if it less than zero then this block is executed
+        console.log("Number is Negative")
+}
