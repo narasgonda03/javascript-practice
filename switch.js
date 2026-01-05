4f08f0f6fbad8c1fac0d0d64e2da64fd9261fed1
Narasgonda Patil <narasgondapatil0345@gmail.com>
2026-01-05 18:14:56 +0530

    Switch datatype program

---
+++ switch.js
@@ -0,0 +1,11 @@
+let day = "Monday"
+switch(day){//used to check multiple values of day 
+    case "Sunday":
+        console.log("Holiday of the week")
+        break;
+    case "Monday":
+        console.log("Start of the week")
+        break;
+    default:
+        console.log("Normal day of the week")
+}
