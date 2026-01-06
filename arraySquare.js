78ce6b1c21d1f5e43e811f8e83015c618217c3cd
Narasgonda Patil <narasgondapatil0345@gmail.com>
2026-01-06 17:13:17 +0530

    Change regular array to squared array

---
+++ arraySquare.js
@@ -0,0 +1,11 @@
+let array = [1,2,3,4,5]
+let squared = array.map(arr=>arr*arr)
+console.log(squared)
+
+let [a,b] = [10,20]//multiple value assign to multiple variable
+console.log(a+b)
+
+let arr1 = [1,2,3]
+let arr2 = [arr1,4,5,6]//add arr1 values to arr2
+console.log(arr2)
+
