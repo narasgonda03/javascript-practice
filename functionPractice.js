ea7d8068b739e5bc0763aba9a9f746e9774df7b5
Narasgonda Patil <narasgondapatil0345@gmail.com>
2026-01-06 13:33:30 +0530

    Fundamental of Function is complete

---
+++ functionPractice.js
@@ -0,0 +1,20 @@
+let divide = function(a,b){//store function in add variable
+    return a/b
+}
+console.log(divide(10,2))//call only the add variable
+
+const mul=(a,b)=>a*b;//create function mul using arrow function
+console.log(mul(10,5))
+
+//default parameter
+function greet(name = "Aakash"){ //default parameter in function
+    console.log(`Hello, ${name}!`);  //strictly use "`" in between parenthesis.
+}
+greet();
+greet(name="Narasgonda");
+
+//rest parameter --> multiple argument passed as array
+function sum(...numbers) {
+return numbers.reduce((acc, num) => acc + num, 0);
+}
+console.log(sum(2, 4, 6, 8)); // Output: 20
