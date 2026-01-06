88cc7ecb63ab7e738ccf43f47899dda82947210d
Narasgonda Patil <narasgondapatil0345@gmail.com>
2026-01-06 13:09:25 +0530

    Comment is added

--- squareFunction.js
+++ squareFunction.js
@@ -1,8 +1,8 @@
-function square(a){
-    return a * a;
+function square(a){//use function keyword to declare a function 
+    return a * a;//return result of the given function square
 }
 function add(a,b){
     return a + b
 }
-console.log("Square of Number :",square(5))
-console.log("Addition of Two Number:",add(5,10))
+console.log("Square of Number :",square(5))//call and print the square function
+console.log("Addition of Two Number:",add(5,10))//call and print the add function
