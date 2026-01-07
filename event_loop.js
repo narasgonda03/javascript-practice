bb73cde9c2c008eb4f189db252be62543c9a919b
Narasgonda Patil <narasgondapatil0345@gmail.com>
2026-01-07 17:43:01 +0530

    Basic even loop progra is added

---
+++ event_loop.js
@@ -0,0 +1,9 @@
+console.log("Start"); //first line execute
+
+setTimeout(() => {
+  console.log("Timeout"); //It is in web api env,
+  //after time complete it go into callback queue then event loop check callstack is empty
+  //if it empty then push to callstack then Timeout is print on console.
+}, 0);
+
+console.log("End"); //second line
